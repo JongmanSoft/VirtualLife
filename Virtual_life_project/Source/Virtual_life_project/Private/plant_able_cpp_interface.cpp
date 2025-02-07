9ab2bb3a6b2a4270ec2ae17b8441f0579599a893
코딩천재이종만 <jongman@tukorea.ac.kr>
2025-02-07 19:16:47 +0900

    일단 인터페이스는 만들었는데...
    왜 적용이 안될꼬~~??

---
+++ Virtual_life_project/Source/Virtual_life_project/Private/plant_able_cpp_interface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "plant_able_cpp_interface.h"
+
+// Add default functionality here for any Iplant_able_cpp_interface functions that are not pure virtual.
