08fa0fbf22099bdf663433cbed74e991b838ff65
코딩천재이종만 <jongman@tukorea.ac.kr>
2025-02-20 19:11:44 +0900

    흠,, 커스텀데이터 업데이트용 c++클래스추가

---
+++ Virtual_life_project/Source/Virtual_life_project/Public/Custom_data.h
@@ -0,0 +1,21 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "UObject/NoExportTypes.h"
+#include "Custom_data.generated.h"
+
+/**
+ * 
+ */
+UCLASS(ClassGroup = (Custom), meta = (BlueprintSpawnableComponent))
+class VIRTUAL_LIFE_PROJECT_API UCustom_data : public UObject
+{
+	GENERATED_BODY()
+	
+public:
+
+
+
+};
