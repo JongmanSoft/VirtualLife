b1034f30dda4bc1bf0d9392247b37a547f081c89
코딩천재이종만 <jongman@tukorea.ac.kr>
2025-02-22 00:14:34 +0900

    하,,,,
    할수잇을까 ㅠㅠ

---
+++ Virtual_life_project/Source/Virtual_life_project/Private/m_CustomizableSkeletalComponent.cpp
@@ -0,0 +1,52 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "m_CustomizableSkeletalComponent.h"
+
+Um_CustomizableSkeletalComponent::Um_CustomizableSkeletalComponent() 
+{
+	// C++ 방식
+	//UCustomizableObject* CO = LoadObject<UCustomizableObject>(nullptr, TEXT("/Game/custom_system/CO_metahuman.CO_metahuman"));
+	//UCustomizableObjectInstance* COInstance = UCustomizableObjectSystem::GetInstance()->CreateInstance(CO);
+	skin = 0.5;
+	shirt = 0;
+	pants = 0;
+	shoes = 0;
+	R_eye_color_hue = 1;
+	R_eye_color_sat = 1;
+	L_eye_color_hue = 1;
+	L_eye_color_sat = 1;
+	eye_scale = 0.35;
+	pupil_scale = 0.9;
+	hair = 0;
+	hair_color_R = 1;
+	hair_color_G = 0.2;
+	hair_color_B = 1;
+}
+
+void Um_CustomizableSkeletalComponent::custom_data_update(Customizing cus)
+{
+
+
+	skin =        cus.skin  ;
+	shirt =       cus.shirt ;
+	pants =       cus.pants ;
+	shoes =       cus.shoes ;
+	R_eye_color_hue =   cus.R_eye_color_hue;
+	R_eye_color_sat =   cus.R_eye_color_sat;
+	L_eye_color_hue =   cus.L_eye_color_hue;
+	L_eye_color_sat =   cus.L_eye_color_sat;
+	eye_scale =     cus.eye_scale;
+	pupil_scale =   cus.pupil_scale;
+	hair =          cus.hair;
+	hair_color_R =  cus.hair_color_R;
+	hair_color_G =  cus.hair_color_G;
+	hair_color_B =  cus.hair_color_B;
+
+	apply_actor_custom(); //변경 내용을 액터에 반영
+}
+
+void Um_CustomizableSkeletalComponent::apply_actor_custom()
+{
+
+}
