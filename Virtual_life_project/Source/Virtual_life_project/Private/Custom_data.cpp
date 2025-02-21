f2617ae7bdcdea6b2b456b4a604954686ba23316
코딩천재이종만 <jongman@tukorea.ac.kr>
2025-02-21 12:44:52 +0900

    커스텀값 초기값 설정

--- Virtual_life_project/Source/Virtual_life_project/Private/Custom_data.cpp
+++ Virtual_life_project/Source/Virtual_life_project/Private/Custom_data.cpp
@@ -3,3 +3,20 @@
 
 #include "Custom_data.h"
 
+UCustom_data::UCustom_data()
+{
+	skin = 0.5;
+	shirt = 0;
+	pants =0;
+	shoes =0;
+	R_eye_color_hue=1;
+	R_eye_color_sat=1;
+	L_eye_color_hue=1;
+	L_eye_color_sat=1;
+	eye_scale= 0.35;
+	pupil_scale= 0.9;
+	hair=0;
+	hair_color_R=1;
+	hair_color_G=0.2;
+	hair_color_B=1;
+}
--- Virtual_life_project/Source/Virtual_life_project/Public/Custom_data.h
+++ Virtual_life_project/Source/Virtual_life_project/Public/Custom_data.h
@@ -16,6 +16,28 @@ class VIRTUAL_LIFE_PROJECT_API UCustom_data : public UObject
 	
 public:
 
+	UCustom_data();
+
+public:
+	//커스텀 데이터들
+	float skin; // 피부색
+	unsigned short shirt;
+	unsigned short pants;
+	unsigned short shoes;
+	//눈정보 
+	float R_eye_color_hue;
+	float R_eye_color_sat;
+	float L_eye_color_hue;
+	float L_eye_color_sat;
+	float eye_scale;
+	float pupil_scale;
+	//헤어정보
+	unsigned short hair;
+	float hair_color_R;
+	float hair_color_G;
+	float hair_color_B;
+
+
 
 
 };
