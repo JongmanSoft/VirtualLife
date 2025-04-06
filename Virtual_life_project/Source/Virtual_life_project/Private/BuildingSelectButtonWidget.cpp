5875b203923122476f05b08e89f4d7696060f53f
BATMINJI <minjiconan@naver.com>
2025-04-06 11:55:13 +0900

    뭔가 이상함... ㅅㅂ 다시 돌아가기

--- Virtual_life_project/Source/Virtual_life_project/Private/BuildingSelectButtonWidget.cpp
+++ Virtual_life_project/Source/Virtual_life_project/Private/BuildingSelectButtonWidget.cpp
@@ -2,68 +2,8 @@
 
 
 #include "BuildingSelectButtonWidget.h"
-#include "Components/Button.h"
-#include "Components/Image.h"
-#include "Kismet/GameplayStatics.h"
-#include "PlacementActor.h"
 
-void UBuildingSelectButtonWidget::NativeConstruct()
+void UBuildingSelectButtonWidget::OnBuildButtonClicked_Implementation()
 {
-    Super::NativeConstruct();
-
-    if (Build_BTN)
-    {
-        Build_BTN->OnClicked.AddDynamic(this, &UBuildingSelectButtonWidget::OnBuildButtonClicked);
-    }
-
-    if (DataTable && AssetImage)
-    {
-        const FBuildInfo* Info = DataRow.GetRow<FBuildInfo>("PreConstruct_Load");
-        if (Info && Info->Image)
-        {
-            AssetImage->SetBrushFromTexture(Info->Image);
-        }
-    }
-}
-
-void UBuildingSelectButtonWidget::OnBuildButtonClicked()
-{
-    CheckActive();
-
-    if (!DataTable || !PlacementActorClass) return;
-
-    const FBuildInfo* Info = DataRow.GetRow<FBuildInfo>("Click_Spawn");
-    if (!Info || !Info->Mesh) return;
-
-    UWorld* World = GetWorld();
-    if (!World) return;
-
-    // PlacementActor 스폰
-    APlacementActor* NewActor = World->SpawnActor<APlacementActor>(
-        PlacementActorClass,
-        FVector::ZeroVector,
-        FRotator::ZeroRotator
-    );
-
-    if (NewActor)
-    {
-        NewActor->SetMesh(Info->Mesh);
-    }
-}
-
-void UBuildingSelectButtonWidget::CheckActive()
-{
-    UWorld* World = GetWorld();
-    if (!World) return;
-
-    TArray<AActor*> FoundActors;
-    UGameplayStatics::GetAllActorsOfClass(World, APlacementActor::StaticClass(), FoundActors);
-
-    for (AActor* Actor : FoundActors)
-    {
-        if (IsValid(Actor))
-        {
-            Actor->Destroy();
-        }
-    }
+    // 블루프린트에서 구현
 }
--- Virtual_life_project/Source/Virtual_life_project/Public/BuildingSelectButtonWidget.h
+++ Virtual_life_project/Source/Virtual_life_project/Public/BuildingSelectButtonWidget.h
@@ -11,37 +11,13 @@
 /**
  * 
  */
-class UButton;
-class UImage;
-class APlacementActor;
 
 UCLASS()
 class VIRTUAL_LIFE_PROJECT_API UBuildingSelectButtonWidget : public UUserWidget
 {
 	GENERATED_BODY()
 public:
-    virtual void NativeConstruct() override;
-
-    UPROPERTY(meta = (BindWidget))
-    UImage* AssetImage;
-
-    UPROPERTY(meta = (BindWidget))
-    UButton* Build_BTN;
-
-    // DataTable 및 선택 Row
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Build Data")
-    UDataTable* DataTable;
-
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Build Data")
-    FDataTableRowHandle DataRow;
-
-    // 스폰할 액터 클래스
-    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Build Data")
-    TSubclassOf<APlacementActor> PlacementActorClass;
-
-protected:
-    UFUNCTION()
+    UFUNCTION(BlueprintCallable, BlueprintNativeEvent, Category = "Build")
     void OnBuildButtonClicked();
-
-    void CheckActive();
+    virtual void OnBuildButtonClicked_Implementation();
 };
