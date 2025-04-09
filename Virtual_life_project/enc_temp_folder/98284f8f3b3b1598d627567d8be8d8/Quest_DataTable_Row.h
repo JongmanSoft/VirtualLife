8877cdd568bfa3666e2222bedb0f88e14f884197
코딩천재이종만 <jongman@tukorea.ac.kr>
2025-04-10 01:44:41 +0900

    퀘스트 데이터테이블추가, 아이템들 가격추가

---
+++ Virtual_life_project/enc_temp_folder/98284f8f3b3b1598d627567d8be8d8/Quest_DataTable_Row.h
@@ -0,0 +1,39 @@
+// Quest_DataTable_Row.h
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/DataTable.h"
+#include "Quest_DataTable_Row.generated.h"
+
+USTRUCT(BlueprintType)
+struct FRequirItem
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+    uint8 Item_ID;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+    uint8 RequirNum;
+};
+
+USTRUCT(BlueprintType)
+struct FQuestDataRow : public FTableRowBase
+{
+    GENERATED_BODY()
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+    bool IsMain = false;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+    FString Quest_Name;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+    FString Quest_Desc;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+    TArray<FRequirItem> RequirValues;
+
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Quest")
+    TArray<int32> RewardGold;
+};
