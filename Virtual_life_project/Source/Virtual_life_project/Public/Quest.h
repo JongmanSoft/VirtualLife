8494b8b8eb7c523a127fb69c95b3d19e8b742106
코딩천재이종만 <jongman@tukorea.ac.kr>
2025-04-14 13:33:41 +0900

    퀘스트 완료처리

--- Virtual_life_project/Source/Virtual_life_project/Public/Quest.h
+++ Virtual_life_project/Source/Virtual_life_project/Public/Quest.h
@@ -7,6 +7,8 @@
 #include "Quest_DataTable_Row.h"
 #include "Quest.generated.h"
 
+DECLARE_DYNAMIC_MULTICAST_DELEGATE(FOnChangeQuestComplete);
+
 struct requir
 {
     uint8 requir_item_ID;
@@ -18,6 +20,9 @@ UCLASS(Blueprintable)
 class VIRTUAL_LIFE_PROJECT_API UQuest : public UObject
 {
     GENERATED_BODY()
+public:
+    UPROPERTY(BlueprintAssignable)
+    FOnChangeQuestComplete OnChangeQuestComplete;
 
 public:
  //데이터 테이블
@@ -26,8 +31,11 @@ public:
     FQuestDataRow Quest_info;
     //현재 진행정보
     TArray<requir> Goals;
+    //완료를 나타내는 bool
+    bool complete;
 public:
     UQuest();
     void init_quest(const uint8& Quest_ID); //아이디를 주면 맞는퀘스트생성
     bool quest_success();
+    void set_complete(const bool& com);
 };
