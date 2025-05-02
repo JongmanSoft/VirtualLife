789b1130c73606c512e948eb4e7fd4c5e6d653b4
BATMINJI <minjiconan@naver.com>
2025-05-02 21:40:37 +0900

    벽용 placement actor 클래스 추가

---
+++ Virtual_life_project/Source/Virtual_life_project/Public/WallPlacementActor.h
@@ -0,0 +1,30 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlacementActor.h"
+#include "WallPlacementActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTUAL_LIFE_PROJECT_API AWallPlacementActor : public APlacementActor
+{
+	GENERATED_BODY()
+	
+public:
+    virtual void Tick(float DeltaTime) override;
+    virtual void BeginPlay() override;
+
+    void SetStartPoint(const FVector& InStart);
+    void UpdateWallPreview(const FVector& Current);
+
+    void ConfirmWall(); // 최종 벽 생성
+    void CancelWall(); // 임시 벽 삭제
+
+private:
+    FVector StartPoint;
+    bool bHasStart = false;
+};
---
+++ Virtual_life_project/enc_temp_folder/8d6bf8fd5450bdc72edaf6377de5fc6/WallPlacementActor.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "PlacementActor.h"
+#include "WallPlacementActor.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTUAL_LIFE_PROJECT_API AWallPlacementActor : public APlacementActor
+{
+	GENERATED_BODY()
+	
+};
