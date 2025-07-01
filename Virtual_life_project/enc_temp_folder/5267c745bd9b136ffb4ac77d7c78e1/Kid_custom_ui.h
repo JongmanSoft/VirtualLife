39fa1282ed4d148e4fa322ad6b30d54a187e820c
코딩천재이종만 <jongman@tukorea.ac.kr>
2025-07-02 02:45:49 +0900

    아이만들기 ui 클래스

---
+++ Virtual_life_project/enc_temp_folder/5267c745bd9b136ffb4ac77d7c78e1/Kid_custom_ui.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Kid_custom_ui.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class VIRTUAL_LIFE_PROJECT_API UKid_custom_ui : public UUserWidget
+{
+	GENERATED_BODY()
+	
+};
