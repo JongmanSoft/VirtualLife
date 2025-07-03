a1da3d10e86853b266f1fa8600c25f1faee0d93c
BATMINJI <minjiconan@naver.com>
2025-07-03 21:55:14 +0900

    character interaction C++ 변환 완

---
+++ Virtual_life_project/Source/Virtual_life_project/Private/Character_interface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character_interface.h"
+
+// Add default functionality here for any ICharacter_interface functions that are not pure virtual.
