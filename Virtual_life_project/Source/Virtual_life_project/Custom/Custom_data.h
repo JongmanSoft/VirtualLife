a8994940ae81f711863c3184cb1e2446597731ef
코딩천재이종만 <jongman@tukorea.ac.kr>
2025-07-14 08:40:29 +0900

    안쓰고 잇던 커스텀데이터를 재활용해봅시더
    이거슨 자식용으로 쓸겁니두

--- Virtual_life_project/Source/Virtual_life_project/Custom/Custom_data.h
+++ Virtual_life_project/Source/Virtual_life_project/Custom/Custom_data.h
@@ -20,39 +20,65 @@ public:
 
 public:
 	//커스텀 데이터들
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "SkinData")
 	float skin; // 피부색
-	unsigned short shirt;
-	unsigned short pants;
-	unsigned short shoes;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ClothigData")
+	uint8 shirt;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ClothigData")
+	uint8 pants;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "ClothigData")
+	uint8 shoes;
 	//눈정보 
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EyeData")
 	float R_eye_color_hue;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EyeData")
 	float R_eye_color_sat;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EyeData")
 	float L_eye_color_hue;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EyeData")
 	float L_eye_color_sat;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EyeData")
 	float eye_scale;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EyeData")
 	float pupil_scale;
 	//헤어정보
-	unsigned short hair;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HairData")
+	uint8 hair;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HairData")
 	float hair_color_R;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HairData")
 	float hair_color_G;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "HairData")
 	float hair_color_B;
 
 	//얼굴 커스텀
 	//눈
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EyeData")
 	float eye_width;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EyeData")
 	float eye_thick;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "EyeData")
 	float eye_slope;
 	//코
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NoseData")
 	float nose_width;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "NoseData")
 	float nose_height;
 	//입
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MouseData")
 	float mouse_width;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MouseData")
 	float mouse_thick;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "MouseData")
 	float mouse_slope;
 	//얼굴형
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FaceData")
 	float chin;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FaceData")
 	float jaw;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FaceData")
 	float heavy;
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "FaceData")
 	float face_width;
 
 	//아이브로우, 마스카라
