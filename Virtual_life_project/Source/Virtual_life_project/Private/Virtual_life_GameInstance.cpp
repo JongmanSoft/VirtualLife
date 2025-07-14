08fb19a24a17915fd1af46ff6d024aed345560ff
BATMINJI <minjiconan@naver.com>
2025-07-14 16:41:21 +0900

    푸시할 때 겹쳐졌삼

--- Virtual_life_project/Source/Virtual_life_project/Private/Virtual_life_GameInstance.cpp
+++ Virtual_life_project/Source/Virtual_life_project/Private/Virtual_life_GameInstance.cpp
@@ -206,26 +206,6 @@ void UVirtual_life_GameInstance::SendPartyRejectPacket(const FString& Id_str)
 	SendEnqueue(&p, p.size);
 }
 
-void UVirtual_life_GameInstance::SendVoicePacket(uint8* data, int32 length)
-{
-	while (length > 0)
-	{
-		int32 chunk = FMath::Min(length, 512);
-
-		CS_VOICE_CHAT_PACKET p;
-		p.size = sizeof(uint16) + sizeof(PACKETID) + sizeof(uint32) + sizeof(uint16) + chunk;
-		p.type = CS_VOICE_CHAT;
-		p.from_id = MyPlayerInfo.id;
-		p.data_len = chunk;
-		FMemory::Memcpy(p.data, data, chunk);
-
-		SendEnqueue(&p, p.size);
-
-		data += chunk;
-		length -= chunk;
-	}
-}
-
 bool UVirtual_life_GameInstance::SendEnqueue(void* packet, int32 PacketSize)
 {
 	TArray<uint8> PacketData;
@@ -660,14 +640,6 @@ void UVirtual_life_GameInstance::ProcessRecvPackets()
 
 			enter_time = p.time;
 			UGameplayStatics::OpenLevel(this, FName(TEXT("OpenWorldMap")));
-			break;
-		}
-		case SC_VOICE_CHAT:
-		{
-			SC_VOICE_CHAT_PACKET p;
-			FMemory::Memcpy(&p, PacketData.GetData(), sizeof(SC_VOICE_CHAT_PACKET));
-
-			AVL_Player* TargetPlayer = dynamic_cast<AVL_Player*>(OtherPlayers[p.from_id].character);
 			break;
 		}
 		case SC_RESULT_PARTY:
