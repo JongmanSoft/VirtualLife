a37cf537ea0f2ef523453c379eb63221bfb01c60
D4LGONA(asus) <laff3y2297@gmail.com>
2025-07-16 21:26:09 +0900

    초기화 잘못 함..

--- server/stdafx.cpp
+++ server/stdafx.cpp
@@ -3,9 +3,12 @@
 std::atomic_int id = 0;
 std::array<Player, MAX_PLAYER> players{};
 std::mutex players_mutex;
+std::array<Door, MAX_DOOR> doors{};
 concurrency::concurrent_unordered_map<std::string, Room*> rooms{};
 float f_time;
 std::chrono::high_resolution_clock::time_point startTime;
+
+
 
 int setid()
 {
