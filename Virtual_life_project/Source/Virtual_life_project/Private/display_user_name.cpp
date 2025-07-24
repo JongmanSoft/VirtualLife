d657f8c25028362c6f85231787fb1e66b88d71b4
D4LGONA(asus) <laff3y2297@gmail.com>
2025-07-24 10:49:27 +0900

    컴파일안돼서고침

--- Virtual_life_project/Source/Virtual_life_project/Private/display_user_name.cpp
+++ Virtual_life_project/Source/Virtual_life_project/Private/display_user_name.cpp
@@ -1,7 +1,7 @@
 // Fill out your copyright notice in the Description page of Project Settings.
 
-#include "Components/TextBlock.h"
 #include "display_user_name.h"
+#include "Components/TextBlock.h"
 
 void Udisplay_user_name::set_name(const FText& new_name)
 {
